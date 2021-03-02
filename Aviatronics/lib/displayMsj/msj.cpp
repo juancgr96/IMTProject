5f9fdba316e9aaa13d5767e0dce5303aff23bd62
jorgealcalah <69773106+jorgealcalah@users.noreply.github.com>
2021-03-02 01:00:11 -0600

    Add files via upload

---
+++ Aviatronics/lib/displayMsj/msj.cpp
@@ -0,0 +1,31 @@
+#include "jsjsjs.h"
+
+displayMessage::displayMessage()
+{
+}
+
+displayMessage::~displayMessage()
+{
+}
+
+void displayMessage::dispMes(float lat, float ing)
+{
+  String message;
+  uint8_t speed = gps.speed.kmph();
+  uint8_t sat = gps.satellites.value();
+  Serial.println("MESSAGE:");
+  message.concat(String(lat, 9));
+  message.concat(",");
+  message.concat(String(lng, 9));
+  message.concat(",");
+  message.concat(String(speed, 2));
+  message.concat(",");
+  message.concat(sat);
+  message.concat("Hour:");
+  message.concat(gps.time.hour());
+  message.concat(":");
+  message.concat(gps.time.minute());
+  message.concat(":");
+  message.concat(gps.time.second());
+  Serial.println(message); 
+}
---
+++ Aviatronics/lib/displayMsj/msj.h
@@ -0,0 +1,17 @@
+class displayMessage
+{
+    public:
+    //constructor y destructor
+    displayMessage();
+    ~displayMessage();
+    //método para desplegar mensaje
+    void dispMes(float lat, float ing);
+
+
+    private:
+    float _lat;
+    float _ing;
+
+
+
+};
